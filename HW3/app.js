d7efda9f96523044076a882b431292247c16c855
anricoj1 <anricoj1@southernct.edu>
2019-12-09 13:51:16 -0500

    switch cases for routing..update README

--- HW3/app.js
+++ HW3/app.js
@@ -5,17 +5,31 @@ var fs = require('fs');
 // rootPath
 var rootPath = './views/pages/';
 
+http.createServer(function(req, res) {
+    var parseUrl = url.parse(req.url);
+
+    switch (parseUrl.pathname) {
+        case "/":
+            index(req, res);
+            break;
+        case "/charts":
+            thisChart(req, res);
+            break;
+    }
+}).listen(8080);
+console.log("Server Running at http://localhost:8080");
 
 
-let requestHandler = (req, res) => {
+
+function index(req, res) {
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type' : 'text/html'
     });
 
     fs.readFile(rootPath + 'index.html', null, function(err, data) {
         if (err) {
             res.writeHead(404);
-            res.write("Whoops! File Not Found");
+            res.write("Whoops File Not Found!");
         } else {
             res.write(data);
         }
@@ -23,5 +37,20 @@ let requestHandler = (req, res) => {
     });
 };
 
-http.createServer(requestHandler).listen(8080);
-console.log('Server Running at http://localhost:8080');
+function thisChart(req, res) {
+    res.writeHead(200, {
+        'Content-Type' : 'text/html'
+    });
+
+    fs.readFile(rootPath + 'chart.html', null, function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.write("Whoops File Not Found!");
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+};
+
+
--- README.md
+++ README.md
@@ -1,14 +1,38 @@
 # CSC443 | Internet Programming
 
-this course focuses on Javascript and Nodejs
+This course focuses on Javascript and Nodejs
 
 # HW1 | Pong
+- Simple pong game
+- This is built with canvas.
 
 
 # HW2 | Pacamn
+- Pacman game with no ghosts
+- Make use of switch cases!!
+- Classes:
+    - Movemnt
+    - Pacman extends Movement
+- Canvas not used.
 
 
 # HW3 | Survey Nodejs
+A Survey for CSC 443. Send a survey form and view pie charts
+on data sent.
+
+- npm install
+- npm start
+
+if you have nodemon installed:
+    - npm install
+    - nodemon server
+
+if you dont use nodemon:
+    - This package restarts the server when changes are made.
+    - npm install nodemon -g 
+    - -g is to globally install it on your local machine
+
+
 
 
 
